7111a2dcb51c82f7db8a24cff95960a93016c592
DeclanMcGauley <73233273+DeclanMcGauley1@users.noreply.github.com>
2020-12-07 17:49:25 +0000

    Created the inital scene that will hold the gameboard

---
+++ Checkers/src/Gameboard.java
@@ -0,0 +1,34 @@
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+
+public class Gameboard extends Application{
+	
+	public static final int BOARD_HEIGHT = 8;
+	public static final int BOARD_WIDTH = 8;
+	public static final int TILE_SIZE = 100;
+	
+	
+	
+	private Parent createBoard() {
+		Pane root = new Pane();
+		root.setPrefSize(TILE_SIZE * BOARD_WIDTH, TILE_SIZE * BOARD_HEIGHT);
+		return root;
+	}
+	
+	public void start(Stage primaryStage) throws Exception{
+		Scene scene = new Scene(createBoard());
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Checkers!");
+		primaryStage.show();
+	}
+	
+	
+	public static void main (String[] args) {
+		Application.launch(args);
+	}
+}
